9e36d0c3ae6df53d0c4125a08f937251696e8cd7
João Vitor de Camargo <camargo.dev@gmail.com>
2025-03-03 21:43:32 -0300

    Round engine playing entire round

---
+++ src/game/interface/player_interface.py
@@ -0,0 +1,6 @@
+from src.model.player_hand import Player
+from src.model.card import Card
+
+class PlayerInterface:
+    def select_card(player: Player) -> Card:
+        pass
--- src/game/round_engine.py
+++ src/game/round_engine.py
@@ -2,24 +2,33 @@ from typing import Tuple
 from src.model.player_hand import Player
 from src.model.card import Card
 from src.model.round_data import RoundData
+from src.game.interface.player_interface import PlayerInterface
 
 class RoundEngine:
-    def __init__(self, players: list[Player]):
-        self.round_data = RoundData(players)
+    def __init__(self, player_interface: PlayerInterface):
+        self.player_interface = player_interface
 
-    def player_play_card(self, player: Player, card: Card) -> Player:
-        """Allows a player to play a card. Raises an error if the player has already played."""
-        # Check if the player has already played
-        if player in self.round_data.card_by_player:
-            raise ValueError(f"{player.name} has already played a card.")
+    def play_round(self, players: list[Player], starter_player: Player) -> RoundData:
+        round_data = RoundData(players)
+        cards_played_count = 0
+        while cards_played_count < len(players):
+            current_player = self.__define_player__(players, starter_player, cards_played_count)
+            card = self.player_interface.select_card(current_player)
+            current_player.play_card(card)
+            round_data.add_played_card(current_player, card)
+            cards_played_count += 1
+        return round_data
 
-        player.play_card(card)
-        self.round_data.add_played_card(player, card)
-        return player
+    def __define_player__(self, players: list[Player], starter_player: Player, cards_count: int) -> Player:
+        starter_index = players.index(starter_player)
+        return players[(starter_index + cards_count) % len(players)]
 
-    def get_round_winner(self) -> Tuple[Player, Card] | None:
-        """Returns the player who played the strongest card of the round, along with the card."""
-        if len(self.round_data.card_by_player) != len(self.round_data.players):
-            raise ValueError("Not all players have played a card.")
+    # def player_play_card(self, player: Player, card: Card) -> Player:
+    #     """Allows a player to play a card. Raises an error if the player has already played."""
+    #     if player in self.round_data.card_by_player:
+    #         raise ValueError(f"{player.name} has already played a card.")
 
-        return self.round_data.get_winner()
+    #     player.play_card(card)
+    #     self.round_data.add_played_card(player, card)
+    #     return player
+
